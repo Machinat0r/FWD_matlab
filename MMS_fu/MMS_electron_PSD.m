74a693c4bbd30cfada41ae4448019033de427dae
付文鼎 <fuwending_29@163.com>
2025-06-07 17:57:38 +0800

    Ver 25.6.7

---
+++ MMS_fu/MMS_electron_PSD.m
@@ -0,0 +1,135 @@
+% % Script to plot electron PSD around pitch angles 0, 90, and 180 deg 
+% and PSD versus pitch angle L1b brst data 
+%
+% Written by D. B. Graham
+clear;
+clc;
+mms.db_init('local_file_db','E:\My_work\mms_db\data');
+ic = 3; % Spacecraft number
+
+% Tintr = irf.tint('2019-08-16T09:31:45.0Z/2019-08-16T09:32:15.0Z');
+% 
+
+Tintr = irf.tint('2020-07-15T23:22:22.100Z/2020-07-15T23:22:22.510Z');
+Tintr = irf.tint('2020-07-15T23:22:21.100Z/2020-07-15T23:22:21.510Z');
+% Tintr = irf.tint('2020-07-15T23:22:21.800Z/2020-07-15T23:22:22.800Z');
+% Tintr = irf.tint('2019-08-16T09:31:04.700Z/2019-08-16T09:32:04.960Z');
+%% Load data
+
+tic;
+c_eval('ePDist = mms.get_data(''PDe_fpi_brst_l2'',Tintr,?);',ic)
+c_eval('Bxyz=mms.get_data(''B_gsm_brst_l2'',Tintr,?);',ic);
+c_eval('SCpot=mms.get_data(''V_edp_brst_l2'',Tintr,?);',ic);
+toc;
+ePDist = ePDist.convertto('s^3/km^6');
+SCpot = SCpot.resample(ePDist);
+
+
+
+%% 连续时间平均分布
+
+Paddist(32,24)=0
+%% Produce a single PAD at a selected time
+% for ii=1:length(ePDist.time.epoch)
+time = irf_time(ePDist.time,'epochTT>utc');
+for ii=1:length(time(:,1))
+tint = irf_time(time(ii,:),'utc>epochTT');
+% tint = irf_time('2018-08-22T15:34:33.28Z','utc>epochTT');
+clear paddist;
+clear thetapad;
+clear energypad;
+clear tintpad;
+clear energypad;
+[paddist,thetapad,energypad,tintpad] = mms.get_pitchangledist(ePDist,Bxyz,tint,'angles',24); 
+[~,idx] = min(abs(SCpot.time-tint));
+energypad = energypad-SCpot.data(idx);
+
+Paddist=Paddist+paddist
+
+end
+
+Paddist_aver=Paddist/9
+
+paddistsmooth =[];
+for i=1:32
+%   paddisti = (smooth(paddist(i,:),180))'
+  paddisti = (smoothts(Paddist_aver(i,:),'b',1))'
+  paddistsmooth(i,:)= paddisti
+end
+% paddistsmooth(9,:)=smoothts(paddistsmooth(9,:),'b',15);
+
+
+
+%% Plot PAD
+
+fn=figure;
+set(fn,'Position',[200 200 600 400])
+%     h(1)=axes('position',[0.08 0.12 0.3 0.80]); 
+%     h(1)=axes('position',[0.30 0.10 0.30 0.80]); 
+ h(1)=axes('position',[0.10 0.10 0.30 0.80]); 
+    ud=get(fn,'userdata');
+    ud.subplot_handles=h;
+    set(fn,'userdata',ud);
+    set(fn,'defaultLineLineWidth',2); 
+% 
+ymin = 3*10^-2;
+% ymax = ceil(max(max(log10(paddist))));
+ymax = 70;
+yrange = [ymin ymax];
+% plot(h(1),energypad,paddist(:,1),'k',energypad,mean(paddist(:,[8:9]),2),'r',energypad,paddist(:,16),'b');
+% ylabel(h(1),'f_e (s^3 km^{-6})');
+% xlabel(h(1),'E (eV)')
+% set(h(1),'yscale','log');
+% set(h(1),'xscale','log');
+% axis(h(1),[5 3e4 yrange])
+% set(h(1),'xtick',[1e0 1e1 1e2 1e3 1e4 1e5])
+% irf_legend(h(1),{'0 deg'},[0.91 0.92],'color','k')
+% irf_legend(h(1),{'90 deg'},[0.91 0.84],'color','r')
+% irf_legend(h(1),{'180 deg'},[0.91 0.76],'color','b')    
+
+jetcolor = colormap('jet');
+lll = length(jetcolor(:,1));
+vcolors = floor(lll/length(energypad));
+vcolors = (1:length(energypad))*vcolors;
+
+% paddist=smooth(paddist(:,:),4);
+c_eval('plot(h(1),thetapad,squeeze(paddist(?,:)),''color'',jetcolor(vcolors(?),:));',5);
+hold(h(1),'on');
+
+% c_eval('plot(h(1),thetapad,mean(paddist(?,:)),''color'',jetcolor(vcolors(?),:));', 21:32);
+
+c_eval('plot(h(1),thetapad,squeeze(paddistsmooth(?,:)),''color'',jetcolor(vcolors(?),:));', 14:21);
+hold(h(1),'off')
+ylabel(h(1),'f_e (s^3 km^{-6})');
+xlabel(h(1),'\theta (deg.)')
+set(h(1),'yscale','log');
+axis(h(1),[0 180 yrange])
+set(h(1),'xtick',[0 45 90 135 180])
+irf_zoom(h(1),'y',yrange);
+tintutc = tintpad.utc;
+
+for i=5:length(energypad)
+    a=roundn(energypad(1,:)/1000,-3);
+c_eval('irf_legend(h(1),''? keV'',[1.02 1-(i-4)*0.035],''color'',jetcolor(vcolors(i),:));',a(i));
+end
+% a=roundn(energypad(1,:)/1000,-3);
+% c_eval('irf_legend(h(1),''? keV'',[1.02 1-1*0.03],''color'',jetcolor(vcolors(1),:));',a(1));     
+
+
+% title(h(1),strcat(tintutc(12:23),'UT'));
+title(h(1),strcat(tintutc(12:23),'UT'));
+set(gcf,'color','w');
+
+%  set(gcf,'render','painters');
+% % set(gcf,'render','opengl');
+% % set(gcf,'paperpositionmode','auto')
+% % figname='Figure1';
+% 
+% figname=['Electron_PSD_ver2' '_' num2str(ic) '_' time(ii,1:4) time(ii,6:7) time(ii,9:10) '_' time(ii,12:13) time(ii,15:16) time(ii,18:19) time(ii,21:23)];
+% print(gcf, '-dpng', [figname '.png']);
+% close;
+%%
+paddistsmooth2=paddistsmooth
+save energypad.mat energypad
+save thetapad.mat thetapad
+save paddist2.mat paddistsmooth2
---
+++ MMS_fu/batetron_coolinig.m
@@ -0,0 +1,192 @@
+clc;
+clear;
+%% consequence
+load('thetapad.mat','thetapad');%投掷角
+load('energypad.mat','energypad');%能道
+load('paddist1.mat','paddistsmooth');%PSD
+
+%% source
+load('paddist2.mat','paddistsmooth2');%PSD
+
+ene_chan=energypad';%能道
+pad=thetapad;%投掷角
+% initial PSD
+PSD_ini=paddistsmooth2;
+
+% final PSD
+PSD_fin=paddistsmooth;
+
+%% for good-looking plot
+% Pitang=Pitang_org;  Pitang(1)=0;  Pitang(end)=pi; 
+Pitang=pad./180*pi; 
+Pitang(1)=0;  
+Pitang(end)=pi;
+NEe=length(ene_chan); %12个能段
+Eele=ene_chan;
+NPa=length(pad);%13个投掷角
+
+PSD_i=transpose(log10(PSD_ini));
+
+Eele_par=zeros(NPa,NEe);
+Eele_perp=zeros(NPa,NEe);
+
+for ii=1:NEe
+    for jj=1:NPa
+        Eele_par(jj,ii)=Eele(ii)*cos(Pitang(jj));  %Eele_par=[9x2]
+        Eele_perp(jj,ii)=Eele(ii)*sin(Pitang(jj)); %Eele_per=[9x2]
+    end
+end
+%interpolation for both Eele and PAD
+%interp2 =  returns the interpolated values on a refined grid 
+%   formed by repeatedly dividing the intervals 2 times in each dimension.
+
+Nintp=2;
+Eele_par_intp=interp2(Eele_par,Nintp);%(让值增多) 平行方向能量
+Eele_perp_intp=interp2(Eele_perp,Nintp);% 垂直方向的电子能量
+PSDele_intp=interp2(PSD_i,Nintp);  
+ 
+ 
+
+
+% af=0.96;
+% ab=1.15
+af=0.93;
+ab=1.15
+% af=0.95;
+% ab=1.1
+%First, Fermi accleration, velocity becomes 17/16 of the original one
+FA_factor=af; FA_factor=FA_factor^2;   %Notice Ea=1/2 mv^2
+Eele_par_Fermi=Eele_par_intp*FA_factor;%费米因素乘平行方向的能量
+Eele_perp_Fermi=Eele_perp_intp;% 垂直方向的能量
+
+%Then, betatron accleration, suppose the accleration is multiply 2
+Eele_par_Fb=Eele_par_Fermi;%平行方向的费米能量为垂直平行方向的betatron能量
+Eele_perp_Fb=Eele_perp_Fermi*ab;%垂直方向的能量乘betatron因素为betatron垂直方向的能量
+
+%return to energy and pitch angle
+Eele_Fb=sqrt(Eele_par_Fb.^2+Eele_perp_Fb.^2);%投掷角所对应能段
+Pitang_Fb=acos(Eele_par_Fb./Eele_Fb)*(180/pi);%投掷角
+
+%% Begin plot
+n_subplots=2;
+i_subplot=1;
+set(0,'DefaultAxesFontSize',8);
+set(0,'DefaultLineLineWidth', 0.5);
+fn=figure(10);clf;
+set(gcf,'PaperUnits','centimeters')
+xSize = 16; ySize = 10; coef=floor(min(800/xSize,800/ySize));
+xLeft = (21-xSize)/2; yTop = (30-ySize)/2;
+set(gcf,'PaperPosition',[xLeft yTop xSize ySize])
+set(gcf,'Position',[10 10 xSize*coef ySize*coef])
+
+
+PSDele_intp=10.^PSDele_intp;
+
+% Eele_want=zeros(1,12);
+% Eele_want=[Eele(7) Eele(8) Eele(9) Eele(10)];  % choose energy for comparison
+Eele_want=[ Eele(14) Eele(15) ...
+           Eele(16) Eele(17) Eele(18) Eele(19) Eele(20) Eele(21)];  % choose energy for comparison
+Eele_want(end)=Eele_want(end);
+NEele_want=length(Eele_want);
+
+N_extr=18;
+Pitang_extr=linspace(0,180,N_extr+1);
+Pitang_extplot=linspace(0,180,N_extr);
+
+Eele_Fb_extra=zeros(NEele_want,N_extr);
+PSD_Fb_extra=zeros(NEele_want,N_extr);
+
+Eele_temp=10000000000.0;
+for mm=1:NEele_want %能段
+for ii=1:N_extr  %投掷角
+    for jj=1:length(Pitang_Fb(:,1)) %线性插值得到的能道
+        for kk=1:length(Pitang_Fb(1,:)) %线性插值得到的投掷角
+            if ( (Pitang_Fb(jj,kk)>Pitang_extr(ii)) & (Pitang_Fb(jj,kk)<Pitang_extr(ii+1)) )
+                if (Eele_Fb(jj,kk)>Eele_want(mm))
+                    if (Eele_Fb(jj,kk)<Eele_temp)
+                       Eele_temp=Eele_Fb(jj,kk);
+                       PSD_temp=PSDele_intp(jj,kk); 
+                        pause=1;
+                    end
+                end
+            end
+         end
+    end
+    
+    Eele_Fb_extra(mm,ii)=Eele_temp;
+    PSD_Fb_extra(mm,ii)=PSD_temp;
+    
+    Eele_temp=10000000000.0;
+end
+end
+
+clr=colormap(jet(NEele_want));
+clr=fliplr(clr);
+Pitang=Pitang.*(180/pi);
+
+%% Original distribution
+h(i_subplot)=irf_subplot(1,n_subplots,-i_subplot);i_subplot=i_subplot+1;
+PSD_ini=transpose(PSD_ini);
+for i_E=1:NEele_want
+    xx=pad;
+    yy=PSD_ini(:,(i_E-1)*1+14);  % corresponding energy
+    semilogy(xx,yy, '-', 'LineWidth',1.3, 'color',clr(i_E,:)); % y轴对数坐标
+    set(gca,'Xtick',[0 45 90 135 180]);
+    set(gca,'Xlim',[0 180]);
+    set(gca,'ylim',[1e-2 200]);
+%     set(gca,'ytick',[1e3 1e4]);
+    hold on;
+end
+E_legend=cell(1,NEele_want);
+for i_legend=1:NEele_want
+    E_legend{i_legend}=num2str(Eele(-(i_legend-1)*1+21),3);
+end
+E_legend=wrev(E_legend);
+
+str=['''' E_legend{1} 'eV'','];
+for ii=2:(NEele_want)
+    str=[str ['''' E_legend{ii} 'eV'',']];
+end
+eval(['legend(h(1),' str '''' 'Location' '''' ',' '''' 'Best' '''' ')']);
+legend('boxoff');
+xlabel('Pitch angle [deg]')
+title('origin')
+hold off;
+
+%% Final distribution together with Fermi and Betatron acelariton
+h(i_subplot)=irf_subplot(1,n_subplots,-i_subplot);i_subplot=i_subplot+1;
+PSD_fin=transpose(PSD_fin);
+for i_E=1:NEele_want
+    xx=pad;
+    yy=PSD_fin(:,(i_E-1)*1+14);
+    semilogy(xx,yy, '-', 'LineWidth',1.3, 'color',clr(i_E,:)); 
+    set(gca,'Xtick',[0 45 90 135 180]);
+    set(gca,'Xlim',[0 180]);
+    set(gca,'ylim',[1e-2 200]);
+    hold on;
+end
+% hold off;
+% h(i_subplot)=irf_subplot(1,n_subplots,-i_subplot);i_subplot=i_subplot+1;
+for i_E=1:NEele_want
+    xx=Pitang_extplot;
+    yy=PSD_Fb_extra(i_E,:);
+    semilogy(xx,yy, '--', 'LineWidth',1.3, 'color',clr(i_E,:)); 
+    set(gca,'Xtick',[0 45 90 135 180]);
+    set(gca,'Xlim',[0 180]);
+    set(gca,'ylim',[1e-2 200]);
+    hold on;
+end
+xlabel('Pitch angle [deg]');
+title('final')
+irf_legend(gca,'af =',[0.1 0.1]);
+irf_legend(gca,num2str(af),[0.2 0.1]);
+irf_legend(gca,'ab =',[0.1 0.05]);
+irf_legend(gca,num2str(ab),[0.2 0.05]);
+hold off;
+
+%% save figure
+  set(gcf,'render','painters');%矢量图
+%   set(gcf,'visible','off');
+  figname=['MMS3_PSDmodel_nonaver'];
+ print(gcf, '-dpdf', [figname '.pdf'])
+%   print(gcf, '-dpng', [figname '.png'])
